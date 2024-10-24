2bb727d3554ddbdc40b92f2de19b95309fa8f086
Ole Backhaus <ole.backhaus@gmx.de>
2024-10-24 12:38:08 +0200

    add put method to change data in toDoEntry

--- src/main/java/org/example/todo/ToDoController.java
+++ src/main/java/org/example/todo/ToDoController.java
@@ -10,10 +10,12 @@ import java.util.Optional;
 public class ToDoController {
 
     private final ToDoService toDoService;
+    private final ToDoRepository toDoRepository;
 
     //Inject
-    public ToDoController(ToDoService toDoService) {
+    public ToDoController(ToDoService toDoService, ToDoRepository toDoRepository) {
         this.toDoService = toDoService;
+        this.toDoRepository = toDoRepository;
     }
 
     //Fetch all the todos through the service layer
@@ -35,9 +37,35 @@ public class ToDoController {
         Optional<ToDoEntry> toDoEntry = toDoService.findToDoById(id);
         if (toDoEntry.isPresent()) {
             return ResponseEntity.ok(toDoEntry.get());
+            //Only for Test:
+            //return ResponseEntity.notFound().build();
         } else {
             return ResponseEntity.notFound().build();
         }
 
     }
+
+    @PutMapping("/api/todo/{id}")
+    public ResponseEntity<ToDoEntry> updateToDo(@PathVariable String id, @RequestBody ToDoDTO updatedToDoDTO) {
+        //Check if 2Do exists
+        Optional<ToDoEntry> existingToDoOpt = toDoRepository.findById(id);
+
+        if (existingToDoOpt.isPresent()) {
+            ToDoEntry existingToDo = existingToDoOpt.get();
+
+            //New instance of ToDoEntry with actual data
+            ToDoEntry updatedToDo = new ToDoEntry(existingToDo.id(), updatedToDoDTO.getDescription(),updatedToDoDTO.getStatus());
+
+
+            //Save actual entity in DB
+                toDoRepository.save(updatedToDo);
+
+            //Return actual 2do-entity
+            return ResponseEntity.ok(updatedToDo);
+            } else {
+                //If couldn't find 2do
+                return ResponseEntity.notFound().build();
+            }
+    }
+
 }
